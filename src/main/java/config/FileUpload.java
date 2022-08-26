68358b678fd419221bbf601f1d1df077a1b3ec2d
namhyun99 <sshft@naver.com>
2022-08-26 21:46:24 +0900

    파일업로드 경로

--- src/main/java/config/FileUpload.java
+++ src/main/java/config/FileUpload.java
@@ -1,8 +1,9 @@
 package config;
 
-public class Constants {
+public class FileUpload {
 	//파일 업로드되고 관리되는 디렉토리 
 	public static final String UPLOAD_PATH = "c:\\work_java\\jsp-servlet-project\\src\\main\\webapp\\upload\\";
+	public static final String PRO_UPLOAD_PATH = "c:\\work_java\\jsp-servlet-project\\src\\main\\webapp\\upload\\profile\\";
 	//파일 업로드 용량 제한 (10MB)
 	public static final int MAX_UPLOAD = 10*1024*1024; 
 }
