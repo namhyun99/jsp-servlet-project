feef444a70cb2b2ecef44c4a20179624dd5dce42
namhyun99 <sshft@naver.com>
2022-08-26 21:45:28 +0900

    common js 수정

--- src/main/webapp/Admin/resources/asset/js/common.js
+++ src/main/webapp/Admin/resources/asset/js/common.js
@@ -4,26 +4,16 @@ $(window).one('load', function () {
     $("#load").hide();
 });
 
+//sessionStorage를 이용해 header에서 선언한 contextpath값 받아오기
+function getContextPath() {
+    return sessionStorage.getItem("contextPath");
+}
+
+function getUrl() {
+	return sessionStorage.getItem("url");
+}
+
 $(function () {
-    var path = getContextPath();
-
-    //로그인 버튼 클릭 시
-    $("#btnLogin").click(function () {
-        if (LoginCheck()) {
-            ajaxLogin(path);
-        }
-    });
-
-    //체크박스 클릭 시
-    $("#chkSave").click(function () {
-        checkSaveId();
-    });
-
-    //ID,PW찾기 클릭시
-    $("#remindIdPw").click(function () {
-        remindIdPwOpen();
-    })
-
     //탭메뉴 구현
     $(".tab-menu li").click(function () {
         var activeTab = $(this).attr('data-tab');
@@ -34,279 +24,67 @@ $(function () {
         $(".result-id").text("");
         $(".result-pwd").text("");
     });
-
-
-    //아이디찾기 버튼
-    $("#btnRemindId").click(function () {
-        if (remindIDCheck()) {
-            ajaxRemindID(path);
-        }
-    });
-
-    //비밀번호 찾기 버튼
-    $("#btnRemindPw").click(function () {
-        if (remindPwdCheck()) {
-            ajaxRemindPwd(path);
-        }
-    });
 });
 
 
-//sessionStorage를 이용해 header에서 선언한 contextpath값 받아오기
-function getContextPath() {
-    return sessionStorage.getItem("contextPath");
+//페이지네이션
+function pagination(page, order, searchkey, keyword){
+	var url = getUrl();
+	
+	console.log(order);
+	console.log(searchkey);
+	console.log(keyword);
+	
+//	
+//	if(keyword == ""){
+//		location.href = url + '?order=' +order +"&page=" + page;
+//	} else {
+//		location.href = url + "?page=" + page;
+//	}
+	
+
 }
 
-
-//아이디 찾기 입력 체크
-function remindIDCheck() {
-    var name = $("#name").val();
-    var phone2 = $("#phone2").val();
-
-
-    if (name == "") {
-        $(".result-id").text("이름을 입력해주세요.");
-        $(".input-name-box").addClass("error");
-        $("#name").focus();
-        return false;
-    } else {
-        $(".input-name-box").removeClass("error");
-    }
-
-    if (phone2 == "") {
-        $(".result-id").text("연락처를 입력해주세요.");
-        $(".input-phone-box").addClass("error");
-        $("#phone2").focus();
-        return false;
-    } else {
-        $(".input-phone-box").removeClass("error");
-    }
-
-    var p_exp = /^[0-9]+$/;
-    if (phone2 != "") {
-        if (!p_exp.test(phone2)) {
-            $(".result-id").text("연락처는 숫자로만 입력해주세요.");
-            $(".input-phone-box").addClass("error");
-            $("#phone2").focus();
-            return false;
-        } else {
-            $(".input-phone-box").removeClass("error");
-        }
-    }
-
-    if (phone2.length > 8 || phone2.length <= 7) {
-        $(".result-id").text("연락처는 8개의 숫자로 입력 가능합니다.");
-        $(".input-phone-box").addClass("error");
-        $("#phone2").focus();
-        return false;
-    } else {
-        $(".input-phone-box").removeClass("error");
-    }
-
-    return true;
-}
-
-//아이디 비동기화
-function ajaxRemindID(path) {
-    var name = $("#name").val();
-    var phone = $("#phone1").val() + $("#phone2").val();
-
-    $.ajax({
-        type: "post",
-        url: path + "/admin/actionID",
-        data: { "name": name, "phone": phone },
-        success: function (data) {
-            if (data != "null") {
-                $(".result-id").text("아이디는 [ " + data + " ] 입니다.");
-                $("#name").val("");
-                $("#phone2").val("");
-            } else {
-                $(".result-id").text("없는 아이디 입니다,");
-                $("#name").val("");
-                $("#phone2").val("");
-            }
-        }
-    });
-}
-
-
-//비밀번호 찾기 입력 체크
-function remindPwdCheck() {
-    var userid = $("#userid").val();
-    var email1 = $("#email1").val();
-    var email2 = $("#email2").val();
-
-    if (userid == "") {
-        $(".result-pwd").text("아이디를 입력해주세요.");
-        $(".input-id-box").addClass("error");
-        $("#userid").focus();
-        return false;
-    } else {
-        $(".input-id-box").removeClass("error");
-    }
-
-    if (userid.length < 4 || userid.length > 15) {
-        $(".result-pwd").text("아이디는 4자이상 15자이하로 입력해야 합니다.");
-        $(".input-id-box").addClass("error");
-        $("#userid").focus();
-        return false;
-    } else {
-        $(".input-id-box").removeClass("error");
-    }
-
-    if (email1 == "") {
-        $(".result-pwd").text("이메일 앞자리를 입력해주세요.");
-        $(".input-email1-box").addClass("error");
-        $("#email1").focus();
-        return false;
-    } else {
-        $(".input-email1-box").removeClass("error");
-    }
-
-    if (email2 == "") {
-        $(".result-pwd").text("이메일 뒷자리를 입력해주세요.");
-        $(".input-email2-box").addClass("error");
-        $("#email2").focus();
-        return false;
-    } else {
-        $(".input-email2-box").removeClass("error");
-    }
-
-    var e2_exp = /^[a-z0-9]{2,}\.[a-z]{2,}$/
-    if (email2 != "") {
-        if (!e2_exp.test(email2)) {
-            $(".result-pwd").text("이메일 형식이 맞지 않습니다.");
-            $(".input-email2-box").addClass("error");
-            $("#email2").focus();
-            return false;
-        } else {
-            $(".input-email2-box").removeClass("error");
-        }
-    }
-
-    return true;
-}
-
-//비밀번호 비동기화 ()data json 형태로 보내기)
-function ajaxRemindPwd(path) {
-    var userid = $("#userid").val();
-    var email = $("#email1").val() + "@" + $("#email2").val();
-
-    $.ajax({
-        type: "post",
-        url: path + "/admin/actionPwd",
-        data: { "userid": userid, "email": email },
-        success: function (data) {
-            if (data != "null") {
-                $(".result-pwd").text("임시비밀번호가 메일로 발송되었습니다.");
-                $("#name").val("");
-                $("#phone2").val("");
-            } else {
-                $(".result-pwd").text("가입되지 않은 아이디 입니다.");
-                $("#name").val("");
-                $("#phone2").val("");
-            }
-        }
-    });
-}
-
-
-
-//로그인 입력체크
-function LoginCheck() {
-    var userid = $("#userid");
-    var passwd = $("#passwd");
-    var chkSave = $("#chkSave");
-
-    if (userid.val() == "") {
-        alert("아이디를 입력하세요");
-        userid.focus();
-        return false;
-    }
-
-    if (passwd.val() == "") {
-        alert("비밀번호를 입력하세요");
-        passwd.focus();
-        return false;
-    }
-
-    // 아이디 저장 체크박스 쿠기 저장	
-    if (chkSave.is(":checked")) {
-        saveCookie(userid.val());
-    } else {
-        saveCookie("");
-    }
-
-    return true;
-}
-
-//로그인 비동기화
-function ajaxLogin(path) {
-    var param = "userid=" + $("#userid").val()
-        + "&passwd=" + $("#passwd").val();
-
-    $.ajax({
-        type: "post",
-        url: path + "/admin/actionLogin",
-        data: param,
-        success: function (data) {
-            console.log(data);
-            if (data === 'false') {
-                alert("아이디 또는 비밀번호가 일치하지 않습니다.");
-            } else {
-                location.href = path + "/admin/main";
-            }
-        },
-        error: function () {
+//리스트 정렬 ajax 통신
+function orderSelect(f){
+	var order = $("#order").val();
+	
+	$.ajax({
+		type : "get",
+		data : { "order": order },
+		success : function(){
+		},
+        error: function () {//에러
             location.href = path + "/error/error.jsp";
         }
-    });
+	});
+}
+
+//검색어 
+function listSearch(f){
+	var searchkey = $("#searchkey").val()
+	var keyword = $("#keyword").val();
+	
+//	if(searchkey == ""){
+//		alert("검색옵션을 선택해주세요");
+//		return false;
+//	}
+//	if(keyword == ""){
+//		alert("검색어를 입력해주세요.");
+//		$("#keyword").focus();
+//		return false;
+//	}
+	
+	$.ajax({
+		type : "get",
+		data : {"searchkey" : searchkey, "keyword" : keyword},
+		success : function(){
+		},
+        error: function () {//에러
+            location.href = path + "/error/error.jsp";
+        }
+	});
 }
 
 
-//아이디 저장 체크박스 
-function checkSaveId() {
-    var chkSave = $("#chkSave");
 
-    if (chkSave.is(":checked")) {
-        chkSave.prop("checked", true);
-    }
-}
-
-//getCookie
-function getCookie(cname) {
-    var cookie = document.cookie + ";";
-    var idx = cookie.indexOf(cname, 0);
-    var val = "";
-    if (idx != -1) {
-        cookie = cookie.substring(idx, cookie.length);
-        begin = cookie.indexOf("=", 0) + 1;
-        end = cookie.indexOf(";", begin);
-        val = cookie.substring(begin, end);
-    }
-    return val;
-}
-
-//setCookie(쿠키변수명, 쿠키값, 유효날짜)
-function setCookie(name, value, days) {
-    var path = getContextPath();
-    var today = new Date();
-    today.setDate(today.getDate() + days);
-    //쿠키변수명=쿠키값; path=저장경로; expires=쿠키유효기간;
-    document.cookie = name + "=" + value + ";path=" + path + ";expires=" + today.toGMTString() + ";"
-}
-
-//쿠키값 저장하기
-function saveCookie(id) {
-    if (id != "") {
-        setCookie("userid", id, 7); //7일
-    } else {
-        setCookie("userid", id, -1); //삭제
-    }
-}
-
-//ID,PW찾기 팝업창 열기
-function remindIdPwOpen() {
-    var path = getContextPath();
-    window.open(path + "/admin/remind", "_blank", "width=600, height=600, left=250, top=80, scrollbars=no, toollbars=no, location=no");
-}
